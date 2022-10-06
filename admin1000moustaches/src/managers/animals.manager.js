ea7a9e0307c46a335782adedd254b6daae3d8bd0
Cédric Derache <c.derache@beapp.fr>
2022-10-06 15:11:12 +0200

    Add fields in animal details

--- admin1000moustaches/src/managers/animals.manager.js
+++ admin1000moustaches/src/managers/animals.manager.js
@@ -1,31 +1,45 @@
-import moment from 'moment';
+import moment from "moment";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 class AnimalsManager {
     static formatAnimal = (animal) => {
-        animal.readable_birth_date = animal.birthdate != null ? moment(animal.birthdate).format("YYYY/MM/DD") : "";
-        animal.readable_entry_date = animal.entry_date != null ? moment(animal.entry_date).format("YYYY/MM/DD") : "";
+        ["birthdate", "entry_date", "exit_date", "death_date"].forEach(
+            (date) => {
+                const animalDate = animal[date];
+                animal[date] = {};
+                animal[date]["rawValue"] = animalDate;
+                animal[date]["readable"] =
+                    animalDate != null
+                        ? moment(animalDate).format("DD/MM/YYYY")
+                        : null;
+                animal[date]["input"] =
+                    animalDate != null
+                        ? moment(animalDate).format("YYYY-MM-DD")
+                        : null;
+            }
+        );
+        console.log(animal);
         return animal;
-    }
-    
+    };
+
     static getAll = () => {
-        return fetch(`${API_URL}/animals`, {"method": "GET"})
-        .then(response => response.json())
-        .then(animals => animals.map(AnimalsManager.formatAnimal))
-        .catch(err => {
-            console.log(err)
-        })
-    }
+        return fetch(`${API_URL}/animals`, { method: "GET" })
+            .then((response) => response.json())
+            .then((animals) => animals.map(AnimalsManager.formatAnimal))
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
     static getById = (id) => {
-        return fetch(`${API_URL}/animals/${id}`, {"method": "GET"})
-        .then(response => response.json())
-        .then(AnimalsManager.formatAnimal)
-        .catch(err => {
-            console.log(err)
-        })
-    }
+        return fetch(`${API_URL}/animals/${id}`, { method: "GET" })
+            .then((response) => response.json())
+            .then(AnimalsManager.formatAnimal)
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 }
 
 export default AnimalsManager;
--- admin1000moustaches/src/pages/AnimalDetailPage.js
+++ admin1000moustaches/src/pages/AnimalDetailPage.js
@@ -1,51 +1,189 @@
-import Page from 'components/Page';
-import React, { useEffect } from 'react';
+import Page from "components/Page";
+import React, { useEffect } from "react";
 import {
     Button,
+    Card,
+    CardBody,
+    CardHeader,
     Col,
     Input,
+    Label,
     Row,
-    Table,
-} from 'reactstrap';
-import AnimalsManager from '../managers/animals.manager';
-import { useState } from 'react';
-import {
-    MdRefresh,
-    MdAssignment
-} from 'react-icons/md';
-import { useParams } from 'react-router';
+} from "reactstrap";
+import AnimalsManager from "../managers/animals.manager";
+import { useState } from "react";
+import { MdRefresh } from "react-icons/md";
 
-function AnimalDetailPage() {
-    // const { animalId } = useParams();
-    const animalId = 1;
+function AnimalDetailPage({ match, ...props }) {
+    const animalId = match.params.id;
     const [animal, setAnimal] = useState([]);
 
     const getAnimal = () => {
         AnimalsManager.getById(animalId)
-        .then(setAnimal)
-    }
+            .then((animal) => {
+                console.log(animal);
+                return animal;
+            })
+            .then(setAnimal);
+    };
+
     useEffect(() => {
         getAnimal();
     }, []);
 
     return (
-      <Page
-        className="AnimalPage"
-        title="Animal"
-        breadcrumbs={[{ name: 'Animals', active: true }, { name: 'Animal', active: false }]}
-      >
-        <Row>
-            <Col xs={{span: 1, offset: 11}}>
-                <Button onClick={getAnimal}><MdRefresh/></Button>
-            </Col>
-        </Row>
+        <Page
+            className="AnimalPage"
+            title="Détail de l'animal"
+            breadcrumbs={[
+                { name: "Animaux", to: "/animals" },
+                { name: "Animal", active: true },
+            ]}
+        >
+            <Row>
+                <Col xs={{ span: 1, offset: 11 }}>
+                    <Button onClick={getAnimal}>
+                        <MdRefresh />
+                    </Button>
+                </Col>
+            </Row>
 
-        <Row>
-            <Col xs={12}>
-                {animal.name}
-            </Col>
-        </Row>
-      </Page>
+            <Card>
+                <CardHeader>
+                    <h2>{animal.name}</h2>
+                </CardHeader>
+                <CardBody>
+                    <Row>
+                        <Col xs={6}>
+                            <Label>Photo</Label>
+                        </Col>
+                        <Col xs={6}>
+                            <Row>
+                                <Col xs={12}>
+                                    <Label>ICAD</Label>
+                                    <Input
+                                        value={animal.icad || "-"}
+                                        readOnly
+                                    />
+                                </Col>
+                                <Col xs={12}>
+                                    <Label>Espèce</Label>
+                                    <Input
+                                        value={animal.species || "-"}
+                                        readOnly
+                                    />
+                                </Col>
+                                <Col xs={12}>
+                                    <Label>Race</Label>
+                                    <Input
+                                        value={animal.race || "-"}
+                                        readOnly
+                                    />
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>
+                            <Label>Date de naissance</Label>
+                            {(animal.birthdate || {}).rawValue == null && (
+                                <Input value="-" readOnly />
+                            )}
+                            {(animal.birthdate || {}).rawValue != null && (
+                                <Input
+                                    type="date"
+                                    value={animal.birthdate.input}
+                                    readOnly
+                                />
+                            )}
+                        </Col>
+                        <Col xs={6}>
+                            <Label>Signes distinctifs</Label>
+                            <Input
+                                value={animal.distinctive_signs || "-"}
+                                readOnly
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>
+                            <Label>Date de PEC</Label>
+                            {(animal.entry_date || {}).rawValue == null && (
+                                <Input value="-" readOnly />
+                            )}
+                            {(animal.entry_date || {}).rawValue != null && (
+                                <Input
+                                    type="date"
+                                    value={animal.entry_date.input}
+                                    readOnly
+                                />
+                            )}
+                        </Col>
+                        <Col xs={6}>
+                            <Label>Lieu de PEC</Label>
+                            <Input
+                                value={animal.place_of_care || "-"}
+                                readOnly
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>
+                            <Label>Raisons de PEC</Label>
+                            <Input
+                                value={animal.reason_for_care || "-"}
+                                readOnly
+                            />
+                        </Col>
+                        <Col xs={6}>
+                            <Label>Informations de PEC</Label>
+                            <Input value={animal.care_infos || "-"} readOnly />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>
+                            <Label>Date de sortie</Label>
+                            {(animal.exit_date || {}).rawValue == null && (
+                                <Input value="-" readOnly />
+                            )}
+                            {(animal.exit_date || {}).rawValue != null && (
+                                <Input
+                                    type="date"
+                                    value={animal.exit_date.input}
+                                    readOnly
+                                />
+                            )}
+                        </Col>
+                        <Col xs={6}>
+                            <Label>Raison de sortie</Label>
+                            <Input value={animal.exit_reason || "-"} readOnly />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>
+                            <Label>Date de décès</Label>
+                            {(animal.death_date || {}).rawValue == null && (
+                                <Input value="-" readOnly />
+                            )}
+                            {(animal.death_date || {}).rawValue != null && (
+                                <Input
+                                    type="date"
+                                    value={animal.death_date.input}
+                                    readOnly
+                                />
+                            )}
+                        </Col>
+                        <Col xs={6}>
+                            <Label>Raison du décès</Label>
+                            <Input
+                                value={animal.death_reason || "-"}
+                                readOnly
+                            />
+                        </Col>
+                    </Row>
+                </CardBody>
+            </Card>
+        </Page>
     );
 }
 export default AnimalDetailPage;
