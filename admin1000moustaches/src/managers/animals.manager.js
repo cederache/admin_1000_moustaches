3c3770182d1171d41f0df4bc0d975c0b7c6afefb
Cédric Derache <c.derache@beapp.fr>
2022-10-06 15:28:02 +0200

    Rename func animalsManager

--- admin1000moustaches/src/managers/animals.manager.js
+++ admin1000moustaches/src/managers/animals.manager.js
@@ -3,30 +3,29 @@ import moment from "moment";
 const API_URL = process.env.REACT_APP_API_URL;
 
 class AnimalsManager {
-    static formatAnimal = (animal) => {
+    static format = (animal) => {
         ["birthdate", "entry_date", "exit_date", "death_date"].forEach(
             (date) => {
-                const animalDate = animal[date];
+                const rawValue = animal[date];
                 animal[date] = {};
-                animal[date]["rawValue"] = animalDate;
+                animal[date]["rawValue"] = rawValue;
                 animal[date]["readable"] =
-                    animalDate != null
-                        ? moment(animalDate).format("DD/MM/YYYY")
+                    rawValue != null
+                        ? moment(rawValue).format("DD/MM/YYYY")
                         : null;
                 animal[date]["input"] =
-                    animalDate != null
-                        ? moment(animalDate).format("YYYY-MM-DD")
+                    rawValue != null
+                        ? moment(rawValue).format("YYYY-MM-DD")
                         : null;
             }
         );
-        console.log(animal);
         return animal;
     };
 
     static getAll = () => {
         return fetch(`${API_URL}/animals`, { method: "GET" })
             .then((response) => response.json())
-            .then((animals) => animals.map(AnimalsManager.formatAnimal))
+            .then((animals) => animals.map(AnimalsManager.format))
             .catch((err) => {
                 console.log(err);
             });
@@ -35,7 +34,7 @@ class AnimalsManager {
     static getById = (id) => {
         return fetch(`${API_URL}/animals/${id}`, { method: "GET" })
             .then((response) => response.json())
-            .then(AnimalsManager.formatAnimal)
+            .then(AnimalsManager.format)
             .catch((err) => {
                 console.log(err);
             });
