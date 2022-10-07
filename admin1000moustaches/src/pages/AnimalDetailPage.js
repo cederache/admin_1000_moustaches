cc83dfc84e69b52b776813de49006408f28a84b4
Cédric Derache <c.derache@beapp.fr>
2022-10-07 11:00:57 +0200

    Fix loading, error detail page state for Animal and Veterinarian

--- admin1000moustaches/src/pages/AnimalDetailPage.js
+++ admin1000moustaches/src/pages/AnimalDetailPage.js
@@ -16,9 +16,10 @@ import { MdRefresh } from "react-icons/md";
 
 function AnimalDetailPage({ match, ...props }) {
     const animalId = match.params.id;
-    const [animal, setAnimal] = useState({});
+    const [animal, setAnimal] = useState(null);
 
     const getAnimal = () => {
+        setAnimal(null);
         AnimalsManager.getById(animalId).then(setAnimal);
     };
 
@@ -26,18 +27,171 @@ function AnimalDetailPage({ match, ...props }) {
         getAnimal();
     }, []);
 
+    let content = <div>Chargement...</div>;
+
     if (animal === undefined) {
-        return (
-            <Page
-                className="AnimalPage"
-                title="Détail de l'animal"
-                breadcrumbs={[
-                    { name: "Animaux", to: "/animals" },
-                    { name: "Animal", active: true },
-                ]}
-            >
-                <div>Animal non trouvé</div>
-            </Page>
+        content = <div>Animal non trouvé</div>;
+    } else if (animal === null) {
+        content = <div>Chargement...</div>;
+    } else {
+        content = (
+            <div>
+                <Row>
+                    <Col xs={{ span: 1, offset: 11 }}>
+                        <Button onClick={getAnimal}>
+                            <MdRefresh />
+                        </Button>
+                    </Col>
+                </Row>
+
+                <Card>
+                    <CardHeader>
+                        <h2>{animal.name}</h2>
+                    </CardHeader>
+                    <CardBody>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Photo</Label>
+                            </Col>
+                            <Col xs={6}>
+                                <Row>
+                                    <Col xs={12}>
+                                        <Label>ICAD</Label>
+                                        <Input
+                                            value={animal.icad || "-"}
+                                            readOnly
+                                        />
+                                    </Col>
+                                    <Col xs={12}>
+                                        <Label>Espèce</Label>
+                                        <Input
+                                            value={animal.species || "-"}
+                                            readOnly
+                                        />
+                                    </Col>
+                                    <Col xs={12}>
+                                        <Label>Race</Label>
+                                        <Input
+                                            value={animal.race || "-"}
+                                            readOnly
+                                        />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Date de naissance</Label>
+                                {(animal.birthdate || {}).input === null && (
+                                    <Input value="-" readOnly />
+                                )}
+                                {(animal.birthdate || {}).input !== null && (
+                                    <Input
+                                        type="date"
+                                        value={animal.birthdate.input}
+                                        readOnly
+                                    />
+                                )}
+                            </Col>
+                            <Col xs={6}>
+                                <Label>Signes distinctifs</Label>
+                                <Input
+                                    type="textarea"
+                                    value={animal.distinctive_signs || "-"}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Date de PEC</Label>
+                                {(animal.entry_date || {}).input === null && (
+                                    <Input value="-" readOnly />
+                                )}
+                                {(animal.entry_date || {}).input !== null && (
+                                    <Input
+                                        type="date"
+                                        value={animal.entry_date.input}
+                                        readOnly
+                                    />
+                                )}
+                            </Col>
+                            <Col xs={6}>
+                                <Label>Lieu de PEC</Label>
+                                <Input
+                                    type="textarea"
+                                    value={animal.place_of_care || "-"}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Raisons de PEC</Label>
+                                <Input
+                                    type="textarea"
+                                    value={animal.reason_for_care || "-"}
+                                    readOnly
+                                />
+                            </Col>
+                            <Col xs={6}>
+                                <Label>Informations de PEC</Label>
+                                <Input
+                                    type="textarea"
+                                    value={animal.care_infos || "-"}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Date de sortie</Label>
+                                {(animal.exit_date || {}).input === null && (
+                                    <Input value="-" readOnly />
+                                )}
+                                {(animal.exit_date || {}).input !== null && (
+                                    <Input
+                                        type="date"
+                                        value={animal.exit_date.input}
+                                        readOnly
+                                    />
+                                )}
+                            </Col>
+                            <Col xs={6}>
+                                <Label>Raison de sortie</Label>
+                                <Input
+                                    type="textarea"
+                                    value={animal.exit_reason || "-"}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Date de décès</Label>
+                                {(animal.death_date || {}).input === null && (
+                                    <Input value="-" readOnly />
+                                )}
+                                {(animal.death_date || {}).input !== null && (
+                                    <Input
+                                        type="date"
+                                        value={animal.death_date.input}
+                                        readOnly
+                                    />
+                                )}
+                            </Col>
+                            <Col xs={6}>
+                                <Label>Raison du décès</Label>
+                                <Input
+                                    type="textarea"
+                                    value={animal.death_reason || "-"}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                    </CardBody>
+                </Card>
+            </div>
         );
     }
 
@@ -50,161 +204,7 @@ function AnimalDetailPage({ match, ...props }) {
                 { name: "Animal", active: true },
             ]}
         >
-            <Row>
-                <Col xs={{ span: 1, offset: 11 }}>
-                    <Button onClick={getAnimal}>
-                        <MdRefresh />
-                    </Button>
-                </Col>
-            </Row>
-
-            <Card>
-                <CardHeader>
-                    <h2>{animal.name}</h2>
-                </CardHeader>
-                <CardBody>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Photo</Label>
-                        </Col>
-                        <Col xs={6}>
-                            <Row>
-                                <Col xs={12}>
-                                    <Label>ICAD</Label>
-                                    <Input
-                                        value={animal.icad || "-"}
-                                        readOnly
-                                    />
-                                </Col>
-                                <Col xs={12}>
-                                    <Label>Espèce</Label>
-                                    <Input
-                                        value={animal.species || "-"}
-                                        readOnly
-                                    />
-                                </Col>
-                                <Col xs={12}>
-                                    <Label>Race</Label>
-                                    <Input
-                                        value={animal.race || "-"}
-                                        readOnly
-                                    />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Date de naissance</Label>
-                            {(animal.birthdate || {}).rawValue === null && (
-                                <Input value="-" readOnly />
-                            )}
-                            {(animal.birthdate || {}).rawValue !== null && (
-                                <Input
-                                    type="date"
-                                    value={animal.birthdate.input}
-                                    readOnly
-                                />
-                            )}
-                        </Col>
-                        <Col xs={6}>
-                            <Label>Signes distinctifs</Label>
-                            <Input
-                                type="textarea"
-                                value={animal.distinctive_signs || "-"}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Date de PEC</Label>
-                            {(animal.entry_date || {}).rawValue === null && (
-                                <Input value="-" readOnly />
-                            )}
-                            {(animal.entry_date || {}).rawValue !== null && (
-                                <Input
-                                    type="date"
-                                    value={animal.entry_date.input}
-                                    readOnly
-                                />
-                            )}
-                        </Col>
-                        <Col xs={6}>
-                            <Label>Lieu de PEC</Label>
-                            <Input
-                                type="textarea"
-                                value={animal.place_of_care || "-"}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Raisons de PEC</Label>
-                            <Input
-                                type="textarea"
-                                value={animal.reason_for_care || "-"}
-                                readOnly
-                            />
-                        </Col>
-                        <Col xs={6}>
-                            <Label>Informations de PEC</Label>
-                            <Input
-                                type="textarea"
-                                value={animal.care_infos || "-"}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Date de sortie</Label>
-                            {(animal.exit_date || {}).rawValue === null && (
-                                <Input value="-" readOnly />
-                            )}
-                            {(animal.exit_date || {}).rawValue !== null && (
-                                <Input
-                                    type="date"
-                                    value={animal.exit_date.input}
-                                    readOnly
-                                />
-                            )}
-                        </Col>
-                        <Col xs={6}>
-                            <Label>Raison de sortie</Label>
-                            <Input
-                                type="textarea"
-                                value={animal.exit_reason || "-"}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Date de décès</Label>
-                            {(animal.death_date || {}).rawValue === null && (
-                                <Input value="-" readOnly />
-                            )}
-                            {(animal.death_date || {}).rawValue !== null && (
-                                <Input
-                                    type="date"
-                                    value={animal.death_date.input}
-                                    readOnly
-                                />
-                            )}
-                        </Col>
-                        <Col xs={6}>
-                            <Label>Raison du décès</Label>
-                            <Input
-                                type="textarea"
-                                value={animal.death_reason || "-"}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                </CardBody>
-            </Card>
+            {content}
         </Page>
     );
 }
--- admin1000moustaches/src/pages/VeterinarianDetailPage.js
+++ admin1000moustaches/src/pages/VeterinarianDetailPage.js
@@ -17,9 +17,10 @@ import SourceLink from "../components/SourceLink";
 
 function VeterinarianDetailPage({ match, ...props }) {
     const vetId = match.params.id;
-    const [veterinarian, setVeterinarian] = useState({});
+    const [veterinarian, setVeterinarian] = useState(null);
 
     const getVeterinarian = () => {
+        setVeterinarian(null);
         VeterinariansManager.getById(vetId).then(setVeterinarian);
     };
 
@@ -27,18 +28,113 @@ function VeterinarianDetailPage({ match, ...props }) {
         getVeterinarian();
     }, []);
 
+    let content = <div>Chargement...</div>;
     if (veterinarian === undefined) {
-        return (
-            <Page
-                className="VeterinarianPage"
-                title="Détail du vétérinaire"
-                breadcrumbs={[
-                    { name: "Vétérinaires", to: "/veterinarians" },
-                    { name: "Vétérinaire", active: true },
-                ]}
-            >
-                <div>Vétérinaire non trouvé</div>
-            </Page>
+        content = <div>Vétérinaire non trouvé</div>;
+    } else if (veterinarian === null) {
+        content = <div>Chargement...</div>;
+    } else {
+        content = (
+            <div>
+                <Row>
+                    <Col xs={{ span: 1, offset: 11 }}>
+                        <Button onClick={getVeterinarian}>
+                            <MdRefresh />
+                        </Button>
+                    </Col>
+                </Row>
+
+                <Card>
+                    <CardHeader>
+                        <h2>{veterinarian.name}</h2>
+                    </CardHeader>
+                    <CardBody>
+                        <Row>
+                            <Col xs={6}>
+                                <Row>
+                                    <Col xs={12}>
+                                        <Label>Téléphone</Label>
+                                        <Input
+                                            value={veterinarian.phone}
+                                            readOnly
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={12}>
+                                        <Label>E-mail</Label>
+                                        <Input
+                                            value={veterinarian.mail}
+                                            readOnly
+                                        />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col xs={6}>
+                                <Label>
+                                    <SourceLink
+                                        link={`https://www.google.com/maps/place/${veterinarian.address}`}
+                                    >
+                                        <span>
+                                            Adresse <MdDirections />
+                                        </span>
+                                    </SourceLink>
+                                </Label>
+                                <Input
+                                    type="textarea"
+                                    value={veterinarian.address}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={3}>
+                                <Label>Gestion des urgences</Label>
+                                <Input
+                                    value={
+                                        veterinarian.emergencies === true
+                                            ? "Oui"
+                                            : veterinarian.emergencies === false
+                                            ? "Non"
+                                            : "NSP"
+                                    }
+                                    readOnly
+                                />
+                            </Col>
+                            <Col xs={9}>
+                                <Label>
+                                    Méthode de confirmation de rendez-vous
+                                </Label>
+                                <Input
+                                    type="textarea"
+                                    value={
+                                        veterinarian.appointment_confirmation_procedure
+                                    }
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <Label>Date de paiement</Label>
+                                <Input
+                                    type="textarea"
+                                    value={veterinarian.invoice_payment_date}
+                                    readOnly
+                                />
+                            </Col>
+                            <Col xs={6}>
+                                <Label>Moyen de paiement</Label>
+                                <Input
+                                    type="textarea"
+                                    value={veterinarian.payment_method}
+                                    readOnly
+                                />
+                            </Col>
+                        </Row>
+                    </CardBody>
+                </Card>
+            </div>
         );
     }
 
@@ -51,101 +147,7 @@ function VeterinarianDetailPage({ match, ...props }) {
                 { name: "Vétérinaire", active: true },
             ]}
         >
-            <Row>
-                <Col xs={{ span: 1, offset: 11 }}>
-                    <Button onClick={getVeterinarian}>
-                        <MdRefresh />
-                    </Button>
-                </Col>
-            </Row>
-
-            <Card>
-                <CardHeader>
-                    <h2>{veterinarian.name}</h2>
-                </CardHeader>
-                <CardBody>
-                    <Row>
-                        <Col xs={6}>
-                            <Row>
-                                <Col xs={12}>
-                                    <Label>Téléphone</Label>
-                                    <Input
-                                        value={veterinarian.phone}
-                                        readOnly
-                                    />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={12}>
-                                    <Label>E-mail</Label>
-                                    <Input value={veterinarian.mail} readOnly />
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col xs={6}>
-                            <Label>
-                                <SourceLink
-                                    link={`https://www.google.com/maps/place/${veterinarian.address}`}
-                                >
-                                    <span>
-                                        Adresse <MdDirections />
-                                    </span>
-                                </SourceLink>
-                            </Label>
-                            <Input
-                                type="textarea"
-                                value={veterinarian.address}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={3}>
-                            <Label>Gestion des urgences</Label>
-                            <Input
-                                value={
-                                    veterinarian.emergencies === true
-                                        ? "Oui"
-                                        : veterinarian.emergencies === false
-                                        ? "Non"
-                                        : "NSP"
-                                }
-                                readOnly
-                            />
-                        </Col>
-                        <Col xs={9}>
-                            <Label>
-                                Méthode de confirmation de rendez-vous
-                            </Label>
-                            <Input
-                                type="textarea"
-                                value={
-                                    veterinarian.appointment_confirmation_procedure
-                                }
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={6}>
-                            <Label>Date de paiement</Label>
-                            <Input
-                                type="textarea"
-                                value={veterinarian.invoice_payment_date}
-                                readOnly
-                            />
-                        </Col>
-                        <Col xs={6}>
-                            <Label>Moyen de paiement</Label>
-                            <Input
-                                type="textarea"
-                                value={veterinarian.payment_method}
-                                readOnly
-                            />
-                        </Col>
-                    </Row>
-                </CardBody>
-            </Card>
+            {content}
         </Page>
     );
 }
