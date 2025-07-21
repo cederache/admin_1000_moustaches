f3189782c1571e93d6d2b24a2138a640c00e00de
Gaetane Seguin <seguin.gaetane@gmail.com>
2025-07-21 16:35:35 +0200

    feat: add test with "pickup person" connected

--- front/cypress/e2e/Com/test.cy.js
+++ front/cypress/e2e/Com/test.cy.js
@@ -1,14 +1,13 @@
 describe('Pole Communication - Dashboard', () => {
-  it("Should show PoleCom cards", () => {
+  beforeEach(() => {
     cy.login('testcom@example.com', 'testcom');
-
+  });
+  it("Should show PoleCom cards", () => {
     cy.get('.card-title').contains('Nombre d\'animaux présents dans l\'association')
     cy.get('.card-title').contains('Nombre d\'animaux adoptés')
   })
 
   it("Should not show other cards", () => {
-    cy.login('testcom@example.com', 'testcom');
-
-    cy.get('.card-title').contains('Nombre d\'animaux présents dans l\'association BIS').should('not.exist')
+    cy.get('.card-title').contains('Nombre de familles d\'accueil disponibles').should('not.exist')
   })
 })
